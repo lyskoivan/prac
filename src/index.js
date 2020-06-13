2c77b9644443540a9e4c4fefec77d6c430fc5542
Ivan <lysko1999i@gmail.com>
2020-06-13 22:54:46 +0300

    added js-webpack starter from luxplanjay

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { Provider } from 'react-redux';
-
-import store from './redux/store';
-
 import './styles.css';
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import reducer from './budgetApp/budgetAppReducers';
-
-const rootReducer = combineReducers({
-  budgetApp: reducer,
-});
-
-export default rootReducer;
